62c94c1101a9b67f64109464ef3d7de75f45d06a
Jfe22 <2181706@my.ipleiria.pt>
2024-02-03 23:23:09 +0000

    auth exceptions

---
+++ smartpackaging/src/main/java/pt/ipleiria/estg/dei/ei/dae/smartpackaging/exceptions/ForbiddenException.java
@@ -0,0 +1,5 @@
+package pt.ipleiria.estg.dei.ei.dae.smartpackaging.exceptions;
+
+public class ForbiddenException extends Exception {
+    public ForbiddenException(String message) { super(message); }
+}
